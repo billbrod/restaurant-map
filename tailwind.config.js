e7008623f7b9a995b394bf688f6ad40d6360efe5
William F. Broderick <billbrod@gmail.com>
2025-08-28 16:22:39 -0400

    cleaned up appearance some

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// this file is ignored by tailwind itself, but needed for lsp-tailwindcss
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
